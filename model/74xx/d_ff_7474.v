1d8bd070260c32a0786545cf813bac9a48945f5a
Ivan Mikhailov <imihajlow@gmail.com>
2020-01-01 22:04:03 +0000

    D flip-flop 7474 model

---
+++ model/74xx/d_ff_7474.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+module d_ff_7474(
+//output
+           q,
+           n_q,
+//input
+           d,
+           cp,
+           n_cd,
+           n_sd);
+    input wire d;
+    input wire cp;
+    input wire n_cd;
+    input wire n_sd;
+    output wire q;
+    output wire n_q;
+
+    reg q_reg;
+    assign #15 q = (n_cd | n_sd) ? q_reg : 1'b1;
+    assign #15 n_q = (n_cd | n_sd) ? ~q_reg : 1'b1;
+
+    always @(posedge cp or negedge n_cd or negedge n_sd) begin
+        if (~n_cd) begin
+            q_reg <= 1'b0;
+        end else if (~n_sd) begin
+            q_reg <= 1'b1;
+        end else begin
+            q_reg <= d;
+        end
+    end
+endmodule
---
+++ model/test/test_7474.v
@@ -0,0 +1,86 @@
+`timescale 1us/1ns
+module test_7474();
+    task assert;
+        input v;
+        if (v !== 1'b1)
+            $fatal;
+    endtask
+
+    wire q;
+    wire n_q;
+    reg d;
+    reg cp;
+    reg n_cd;
+    reg n_sd;
+    d_ff_7474 inst(
+           .q(q),
+           .n_q(n_q),
+           .d(d),
+           .cp(cp),
+           .n_cd(n_cd),
+           .n_sd(n_sd));
+
+    initial begin
+        $dumpfile("test_7474.vcd");
+        $dumpvars;
+
+        cp = 1'b0;
+        n_cd = 1'b1;
+        n_sd = 1'b1;
+        d = 1'b0;
+
+        n_cd = 1'b0;
+        #1
+        assert(q === 1'b0);
+        assert(n_q === 1'b1);
+
+        n_cd = 1'b1;
+        #1
+        assert(q === 1'b0);
+        assert(n_q === 1'b1);
+
+        n_sd = 1'b0;
+        #1
+        assert(q === 1'b1);
+        assert(n_q === 1'b0);
+
+        n_sd = 1'b1;
+        #1
+        assert(q === 1'b1);
+        assert(n_q === 1'b0);
+
+        n_cd = 1'b0;
+        n_sd = 1'b0;
+        #1
+        assert(q === 1'b1);
+        assert(n_q === 1'b1);
+
+        n_cd = 1'b1;
+        n_sd = 1'b1;
+        d = 1'b1;
+        cp = 1'b1;
+        #1
+        assert(q === 1'b1);
+        assert(n_q === 1'b0);
+
+        cp = 1'b0;
+        #1
+        assert(q === 1'b1);
+        assert(n_q === 1'b0);
+
+        d = 1'b0;
+        #1
+        assert(q === 1'b1);
+        assert(n_q === 1'b0);
+
+        cp = 1'b1;
+        #1
+        assert(q === 1'b0);
+        assert(n_q === 1'b1);
+
+        cp = 1'b0;
+        #1
+        assert(q === 1'b0);
+        assert(n_q === 1'b1);
+    end
+endmodule
