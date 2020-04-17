5e5f36da72f180aa354021ab0b36f2a0b2d03f7f
Ivan Mikhailov <imikhailov@anydesk.com>
2020-04-17 19:42:57 +0200

    BCDF implementation begin

---
+++ app/lib/bcdf.asm
@@ -0,0 +1,125 @@
+	; Binary-coded decimal float
+	; Coding:
+	; Byte 0: exponent (signed)
+	; Byte 1: sign (0x00 - positive, 0xff - negative)
+	; Bytes 2-15: mantissa decimal digits
+
+.const exponent = 0
+.const sign = 1
+.const mantissa = 2
+
+	; bcdf_sum - sum two bcdf numbers
+	; arguments: bcdf_sum_a, bcdf_sum_b
+	; result: bcdf_sum_r
+	.export bcdf_sum
+	.export bcdf_sum_a
+	.export bcdf_sum_b
+	.export bcdf_sum_r
+
+	.section data
+bcdf_sum_a: res 16
+bcdf_sum_b: res 16
+bcdf_sum_r: res 16
+bcdf_sum_ret: res 2
+
+	.section text
+bcdf_sum:
+	mov a, ph
+    mov b, a
+    mov a, pl
+    ldi pl, lo(bcdf_sum_ret)
+    ldi ph, hi(bcdf_sum_ret)
+    st a
+    mov a, 0
+    inc pl
+    adc ph, a
+    st b
+
+    ldi pl, lo(bcdf_sum_a + sign)
+    ldi ph, hi(bcdf_sum_a + sign)
+    ldi a
+
+    ldi pl, lo(bcdf_sum_ret)
+    ldi ph, hi(bcdf_sum_ret)
+    ld b
+    mov a, 0
+    inc pl
+    adc ph, a
+    ld a
+    mov ph, a
+    mov a, b
+    mov pl, a
+	jmp
+
+
+
+	; divmod10 - divide by 10
+	; argument: divmod10_arg (byte)
+	; result: divmod10_div (byte), divmod10_mod (byte)
+	.section data
+divmod10_arg: res 1
+divmod10_div: res 1
+divmod10_mod: res 1
+
+divmod10_ret: res 2
+
+	.section text
+divmod10:
+    mov a, ph
+    mov b, a
+    mov a, pl
+    ldi pl, lo(divmod10_ret)
+    ldi ph, hi(divmod10_ret)
+    st a
+    mov a, 0
+    inc pl
+    adc ph, a
+    st b
+
+    ; div = 0
+	ldi pl, lo(divmod10_div)
+    ldi ph, hi(divmod10_div)
+    st a
+
+    ldi pl, lo(divmod10_arg)
+    ldi ph, hi(divmod10_arg)
+    ld b
+
+    ; b = arg in this loop
+divmod10_loop:
+    ; arg -= 10
+    ldi a, 10
+    sub b, a
+    ldi pl, lo(divmod10_finish)
+    ldi ph, hi(divmod10_finish)
+    jc
+
+    ; div += 1
+    ldi pl, lo(divmod10_div)
+    ldi ph, hi(divmod10_div)
+    ld a
+    inc a
+    st a
+
+    ldi pl, lo(divmod10_loop)
+    ldi ph, hi(divmod10_loop)
+    jmp
+
+divmod10_finish:
+	add b, a ; b - mod
+	ldi pl, lo(divmod10_mod)
+	ldi ph, hi(divmod10_mod)
+	st b
+
+    ldi pl, lo(divmod10_ret)
+    ldi ph, hi(divmod10_ret)
+    ld b
+    mov a, 0
+    inc pl
+    adc ph, a
+    ld a
+    mov ph, a
+    mov a, b
+    mov pl, a
+	jmp
+
---
+++ app/lib/bcdf.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+class Bcdf:
+	def __init__(self):
+		self.exp = 0
+		self.sign = False
+		self.man = [0] * 14
+
+	def __str__(self):
+		return "{}{}.{}e{}".format("+" if not self.sign else "-", self.man[0], "".join(str(x) for x in self.man[1:]), self.exp)
+
+def divmod10(a):
+	return divmod(a, 10)
+
+def bcdfNormalize(a):
+	delta = 0
+	for i in range(14):
+		if a.man[i] != 0:
+			break
+		delta += 1
+	if a.exp - delta < -128:
+		delta = a.exp + 128
+	a.exp = a.exp - delta
+	for i in range(14 - delta):
+		a.man[i] = a.man[i + delta]
+	for i in range(14 - delta, 14):
+		a.man[i] = 0
+
+
+def bcdfAdd(a, b):
+	if a.sign != b.sign:
+		b.sign = not b.sign
+		return bcdfSub(a, b)
+	r = Bcdf()
+	r.sign = a.sign
+	expDiff = a.exp - b.exp
+	if expDiff < 0:
+		a,b = b,a
+		expDiff = -expDiff
+	r.exp = a.exp
+	carry = 0
+	for i in reversed(range(14)):
+		s = a.man[i] + carry
+		bi = i - expDiff
+		if bi >= 0:
+			s += b.man[bi]
+		carry, s = divmod10(s)
+		r.man[i] = s
+	if carry > 0:
+		r.exp += 1
+		for i in reversed(range(1,14)):
+			r.man[i] = r.man[i - 1]
+		r.man[0] = carry
+	return r
+
+def bcdfSub(a, b):
+	if a.sign != b.sign:
+		b.sign = not b.sign
+	r = Bcdf()
+	expDiff = a.exp - b.exp
+	r.sign = a.sign
+	if expDiff < 0:
+		a,b = b,a
+		expDiff = -expDiff
+		r.sign = not r.sign
+	elif expDiff == 0:
+		for i in range(14):
+			if a.man[i] > b.man[i]:
+				break
+			elif a.man[i] < b.man[i]:
+				a,b = b,a
+				r.sign = not r.sign
+				break
+	borrow = 1
+	for i in reversed(range(14)):
+		d = a.man[i] + 10 - 1 + borrow
+		bi = i - expDiff
+		if bi >= 0:
+			d = d - b.man[bi]
+		borrow, d = divmod10(d)
+		r.man[i] = d
+	bcdfNormalize(r)
+	return r
+
+
+if __name__ == '__main__':
+	a = Bcdf()
+	a.exp = 0
+	a.man[0] = 1
+	a.man[1] = 5
+
+	b = Bcdf()
+	b.exp = 0
+	b.man[0] = 9
+	b.man[1] = 7
+	b.man[2] = 6
+	print(a)
+	print(b)
+	print(bcdfAdd(b,a))
+
+	b.man[0] = 0
+	b.man[13] = 9
+	b.exp = -127
+	bcdfNormalize(b)
+	print(b)
+
+	a = Bcdf()
+	b = Bcdf()
+	a.exp = 0
+	a.man[0] = 1
+	a.man[1] = 5
+
+	b.exp = -1
+	b.man[0] = 2
+	b.man[1] = 5
+	print()
+	print(a)
+	print(b)
+	print(bcdfSub(a,b))
+	print(bcdfSub(b,a))
+
+	a = Bcdf()
+	b = Bcdf()
+	a.exp = 0
+	a.man[0] = 1
+	a.man[1] = 5
+
+	b.exp = 0
+	b.man[0] = 1
+	b.man[1] = 7
+	print()
+	print(a)
+	print(b)
+	print(bcdfSub(a,b))
+	print(bcdfSub(b,a))
