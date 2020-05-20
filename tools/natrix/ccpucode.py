f15f23dee296f5580fe47f3935f8c7cb8c4f8ec1
Ivan Mikhailov <imihajlow@gmail.com>
2020-05-20 13:56:57 +0200

    Eq and ne

--- tools/natrix/ccpucode.py
+++ tools/natrix/ccpucode.py
@@ -302,6 +302,10 @@ def genBinary(op, resultLoc, src1Loc, src2Loc):
         return _genBoolBinary(resultLoc, src1Loc, src2Loc, "or", "bool({}) or bool({})", lambda a,b: a or b)
     elif op == 'land':
         return _genBoolBinary(resultLoc, src1Loc, src2Loc, "and", "bool({}) and bool({})", lambda a,b: a and b)
+    elif op == 'eq':
+        return genEq(resultLoc, src1Loc, src2Loc)
+    elif op == 'ne':
+        return genNe(resultLoc, src1Loc, src2Loc)
     else:
         if src1Loc.getType() != src2Loc.getType():
             raise ValueError("Incompatible source types: {} and {}".format(src1Loc.getType(), src2Loc.getType()))
@@ -809,6 +813,146 @@ def _genAddPtr(resultLoc, src1Loc, src2Loc):
         raise RuntimeError("Other sizes than 1 and 2 are not supported for pointer indexing")
     return resultLoc, result
 
+def _genEqNeCmp(src1Loc, src2Loc):
+    '''
+    Compare two locations, store result in b (0 if equal, 1 if not)
+    They shouldn't be both constants.
+    '''
+    t = src1Loc.getType()
+    s1 = src1Loc.getSource()
+    s2 = src2Loc.getSource()
+    l1 = src1Loc.getIndirLevel()
+    l2 = src2Loc.getIndirLevel()
+    isWord = t.getSize() == 2
+    result = ''
+    if l1 == 0 or l2 == 0:
+        # var == const
+        if l1 == 0:
+            s1, s2 = s2, s1
+        result = '''
+            ldi pl, lo({0})
+            ldi ph, hi({0})
+            ld b
+        '''.format(s1)
+        if isinstance(s2, int):
+            l = _lo(s2)
+            if l == 0:
+                result += 'mov a, 0\n'
+            else:
+                result += 'ldi a, {}\n'.format(l)
+        else:
+            result += 'ldi a, lo({})\n'.format(s2)
+        result += 'sub b, a\n'
+        if isWord:
+            result += '''
+                inc pl
+                ld pl
+            '''
+            if isinstance(s2, int):
+                h = _hi(s2)
+                if h == 0:
+                    result += 'mov a, 0\n'
+                else:
+                    result += 'ldi a, {}\n'.format(h)
+            else:
+                result += 'ldi a, hi({})\n'.format(s2)
+            result += '''
+                sub a, pl
+                or b, a
+            '''
+    else:
+        if not isWord:
+            result += '''
+                ldi pl, lo({0})
+                ldi ph, hi({0})
+                ld a
+                ldi pl, lo({1})
+                ldi ph, hi({1})
+                ld b
+                sub b, a
+            '''.format(s1, s2)
+        else:
+            result += '''
+                ldi pl, lo({0})
+                ldi ph, hi({0})
+                ld a
+                ldi pl, lo({1})
+                ldi ph, hi({1})
+                ld b
+                sub b, a
+                inc pl
+                ld a
+                ldi pl, lo({0})
+                ldi ph, hi({0})
+                ld pl
+                sub a, pl
+                or b, a
+            '''.format(s1, s2)
+    return result
+
+def genEq(resultLoc, src1Loc, src2Loc):
+    resultLoc = resultLoc.withType(BoolType())
+    assert(resultLoc.getIndirLevel() == 1)
+    if src1Loc.getType() != src2Loc.getType():
+        raise ValueError("Incompatible types: {} and {}".format(src1Loc.getType(), src2Loc.getType()))
+    t = src1Loc.getType()
+    assert(t.getSize() == 1 or t.getSize() == 2)
+    s1 = src1Loc.getSource()
+    s2 = src2Loc.getSource()
+    rs = resultLoc.getSource()
+    l1 = src1Loc.getIndirLevel()
+    l2 = src2Loc.getIndirLevel()
+    isWord = t.getSize() == 2
+    result = '; {} == {}\n'.format(src1Loc, src2Loc)
+    if l1 == 0 and l2 == 0:
+        # const == const
+        if isinstance(s1, int) and isinstance(s2, int):
+            return Value(BoolType(), 0, int(s1 == s2)), result
+        else:
+            return Value(BoolType(), 0, "int(({}) == ({}))".format(s1, s2)), result
+    else:
+        result += _genEqNeCmp(src1Loc, src2Loc)
+    result += '''
+        dec b
+        mov a, 0
+        adc a, 0
+        ldi pl, lo({0})
+        ldi ph, hi({0})
+        st a
+    '''.format(rs)
+    return resultLoc, result
+
+def genNe(resultLoc, src1Loc, src2Loc):
+    resultLoc = resultLoc.withType(BoolType())
+    assert(resultLoc.getIndirLevel() == 1)
+    if src1Loc.getType() != src2Loc.getType():
+        raise ValueError("Incompatible types: {} and {}".format(src1Loc.getType(), src2Loc.getType()))
+    t = src1Loc.getType()
+    assert(t.getSize() == 1 or t.getSize() == 2)
+    s1 = src1Loc.getSource()
+    s2 = src2Loc.getSource()
+    rs = resultLoc.getSource()
+    l1 = src1Loc.getIndirLevel()
+    l2 = src2Loc.getIndirLevel()
+    isWord = t.getSize() == 2
+    result = '; {} == {}\n'.format(src1Loc, src2Loc)
+    if l1 == 0 and l2 == 0:
+        # const == const
+        if isinstance(s1, int) and isinstance(s2, int):
+            return Value(BoolType(), 0, int(s1 != s2)), result
+        else:
+            return Value(BoolType(), 0, "int(({}) != ({}))".format(s1, s2)), result
+    else:
+        result += _genEqNeCmp(src1Loc, src2Loc)
+    result += '''
+        dec b
+        ldi a, 1
+        sbb a, 0
+        ldi pl, lo({0})
+        ldi ph, hi({0})
+        st a
+    '''.format(rs)
+    return resultLoc, result
 
 def genAdd(resultLoc, src1Loc, src2Loc):
     if resultLoc.getType().isUnknown():
