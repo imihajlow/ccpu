7ea9650d9123577b6197d2c10608538c5340b9eb
Ivan Mikhailov <imihajlow@gmail.com>
2020-11-24 09:01:54 +0100

    Error display in ALU test program

--- app/test/alutest.asm
+++ app/test/alutest.asm
@@ -1,22 +1,28 @@
-.section text
+; ALU test program
+; If any of the tests fails, program jumps to one of the fail_* labels and goes into an infinite loop there.
+; Each of the fail labels is aligned by 0x100 starting from 0x1000.
+; This way you can observe the fail "code" on the address bus display and find out which of the tests has failed.
+
+    .section text
     nop
 begin:
     ; check if sub and invert work
     ldi a, 0xa5
     ldi b, 0xa5
-    ldi pl, lo(fail)
-    ldi ph, hi(fail)
-    sub a, b
+    ldi pl, lo(fail_initial)
+    ldi ph, hi(fail_initial)
+    sub a, b ; a = 0x00, b = 0xa5
     jnz
     ldi a, 0xa5
-    sub b, a
+    sub b, a ; a = 0xa5, b = 0x00
     jnz
-    sub b, a
+    sub b, a ; a = 0xa5, b = 0x5b
     jz
-    sub a, b
+    sub a, b ; a = 0x4a, b = 0x5b
     jz
 
 not:
+    ldi ph, hi(fail_not)
     ldi a, 0xa5
     not a
     jz
@@ -27,6 +33,7 @@ not:
     jnz
 
 and:
+    ldi ph, hi(fail_and)
     ldi a, 0xff
     ldi b, 0xa5
     and a, b
@@ -44,6 +51,7 @@ and:
     jc
 
 or:
+    ldi ph, hi(fail_or)
     ldi a, 0xa5
     ldi b, 0x5a
     or a, b
@@ -65,6 +73,7 @@ or:
     jnz
 
 shl:
+    ldi ph, hi(fail_shl)
     ldi a, 0x5a
     shl a
     jz
@@ -83,6 +92,7 @@ shl:
     jnz
 
 shr:
+    ldi ph, hi(fail_shr)
     ldi a, 0xa5
     shr a
     jz
@@ -101,6 +111,7 @@ shr:
     jnz
 
 sar:
+    ldi ph, hi(fail_sar)
     ldi a, 0xa5
     sar a
     jz
@@ -127,6 +138,7 @@ sar:
 
 
 exp:
+    ldi ph, hi(fail_exp)
     ldi a, 0x01
     sar a
     exp a
@@ -145,6 +157,7 @@ exp:
     js
 
 xor:
+    ldi ph, hi(fail_xor)
     ldi a, 0xa5
     ldi b, 0x33
     xor a, b
@@ -156,6 +169,7 @@ xor:
     jnz
 
 add:
+    ldi ph, hi(fail_add)
     ldi a, 0xef
     ldi b, 0x01
     add a, b
@@ -190,6 +204,7 @@ add:
     jnz
 
 adc:
+    ldi ph, hi(fail_adc)
     ldi a, 1
     sar a
     ldi a, 0xff
@@ -236,6 +251,7 @@ adc:
     jnz
 
 sub:
+    ldi ph, hi(fail_sub)
     ldi a, 1
     ldi b, 2
     sub a, b
@@ -259,6 +275,7 @@ sub:
     jnz
 
 sbb:
+    ldi ph, hi(fail_sbb)
     ldi a, 3
     sar a
     ldi b, 0
@@ -293,6 +310,7 @@ sbb:
     jnz
 
 inc:
+    ldi ph, hi(fail_inc)
     ldi a, 0xa5
     inc a
     jz
@@ -314,6 +332,7 @@ inc:
     jnz
 
 dec:
+    ldi ph, hi(fail_dec)
     ldi a, 0xa5
     dec a
     jz
@@ -345,6 +364,7 @@ dec:
     jnz
 
 mov:
+    ldi ph, hi(fail_mov)
     ldi a, 0xa5
     ldi b, 0x5a
     mov a, b
@@ -361,7 +381,98 @@ mov:
     jmp
 
     .align 0x1000
-fail:
-    ldi pl, lo(fail)
-    ldi ph, hi(fail)
+fail_initial:
+    ; a = 0xfe, b = 0x81
+    ldi pl, lo(fail_initial)
+    ldi ph, hi(fail_initial)
+    jmp
+
+    .align 0x100
+fail_not:
+    ldi pl, lo(fail_not)
+    ldi ph, hi(fail_not)
+    jmp
+
+    .align 0x100
+fail_and:
+    ldi pl, lo(fail_and)
+    ldi ph, hi(fail_and)
+    jmp
+
+    .align 0x100
+fail_or:
+    ldi pl, lo(fail_or)
+    ldi ph, hi(fail_or)
+    jmp
+
+    .align 0x100
+fail_shl:
+    ldi pl, lo(fail_shl)
+    ldi ph, hi(fail_shl)
+    jmp
+
+    .align 0x100
+fail_shr:
+    ldi pl, lo(fail_shr)
+    ldi ph, hi(fail_shr)
+    jmp
+
+    .align 0x100
+fail_sar:
+    ldi pl, lo(fail_sar)
+    ldi ph, hi(fail_sar)
+    jmp
+
+    .align 0x100
+fail_exp:
+    ldi pl, lo(fail_exp)
+    ldi ph, hi(fail_exp)
+    jmp
+
+    .align 0x100
+fail_xor:
+    ldi pl, lo(fail_xor)
+    ldi ph, hi(fail_xor)
+    jmp
+
+    .align 0x100
+fail_add:
+    ldi pl, lo(fail_add)
+    ldi ph, hi(fail_add)
+    jmp
+
+    .align 0x100
+fail_adc:
+    ldi pl, lo(fail_adc)
+    ldi ph, hi(fail_adc)
+    jmp
+
+    .align 0x100
+fail_sub:
+    ldi pl, lo(fail_sub)
+    ldi ph, hi(fail_sub)
+    jmp
+
+    .align 0x100
+fail_sbb:
+    ldi pl, lo(fail_sbb)
+    ldi ph, hi(fail_sbb)
+    jmp
+
+    .align 0x100
+fail_inc:
+    ldi pl, lo(fail_inc)
+    ldi ph, hi(fail_inc)
+    jmp
+
+    .align 0x100
+fail_dec:
+    ldi pl, lo(fail_dec)
+    ldi ph, hi(fail_dec)
+    jmp
+
+    .align 0x100
+fail_mov:
+    ldi pl, lo(fail_mov)
+    ldi ph, hi(fail_mov)
     jmp
