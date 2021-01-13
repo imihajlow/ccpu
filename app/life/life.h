499dc7c9e9f5fdbf0e86f7f96979f79617680131
Ivan Mikhailov <imihajlow@gmail.com>
2021-01-13 23:04:50 +0100

    Conway's game of life implementation

---
+++ app/life/life.h
@@ -0,0 +1,5 @@
+#pragma once
+
+import u8 life_init();
+import u8 life_set(u8 x, u8 y, u8 v);
+import u8 life_step_and_render();
--- app/life/quasipixel.h
+++ app/life/quasipixel.h
@@ -3,15 +3,29 @@
 #define QP_WIDTH 80u8
 #define QP_HEIGHT 60u8
 #define QP_SIZE (80u16 * 60u16)
-#define QP_INDEX(x, y) ((u16)(x) + (u16)(y) * (u16)QP_WIDTH)
 
 import u8 qp_init(u8 fg_color, u8 bg_color);
+
+/*
+    Sets the pixel to the value v, but doesn't render.
+ */
+import u8 qp_set(u8 x, u8 y, u8 v);
+
+/*
+    Sets the pixel to the value v and renders it immediately.
+ */
+import u8 qp_set_and_render(u8 x, u8 y, u8 v);
+
+/*
+    Renders all pixels set with qp_set().
+ */
+import u8 qp_render();
+
 import u8 qp_render_all();
-import u8 qp_render_one(u8 x, u8 y);
+
 import u8 qp_set_cursor_enabled(u8 enabled);
 import u8 qp_set_cursor_pos(u8 x, u8 y);
 import u8 qp_move_cursor(s8 dx, s8 dy);
 
-import u8 qp_fb[QP_SIZE];
 import u8 qp_cursor_x;
 import u8 qp_cursor_y;
