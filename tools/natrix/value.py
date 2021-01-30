ad12ecbd8c02577a2a0cb649b982d6615c66b8a7
Ivan Mikhailov <imihajlow@gmail.com>
2021-01-30 20:46:34 +0100

    Natrix: the arrow operator

--- tools/natrix/value.py
+++ tools/natrix/value.py
@@ -125,5 +125,7 @@ class VarTransformerStageTwo(Transformer):
             return val.takeAddress(Location.fromAny(t))
         elif val.data == "member_access":
             return Tree("member_address", val.children, t.meta)
+        elif val.data == "arrow":
+            return Tree("p_arrow", val.children, t.meta)
         else:
             raise SemanticError(Location.fromAny(t), "Cannot get address")
