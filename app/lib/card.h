bf0219e202583ea1e16654563b9acb993af7304c
Ivan Mikhailov <imihajlow@gmail.com>
2021-02-28 22:05:09 +0100

    Memory card + FAT

---
+++ app/lib/card.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define CARD_SUCCESS 0u8
+#define CARD_ERROR_TIMEOUT 1u8
+#define CARD_ERROR_RESET_FAILED 2u8
+#define CARD_ERROR_ACMD_FAILED 3u8
+#define CARD_ERROR_ACMD41_FAILED 4u8
+#define CARD_ERROR_BLOCK_CMD_FAILED 0xffu8
+
+#define CARD_BLOCK_SIZE 512u16
+
+import u8 card_init();
+import u8 card_power_off();
+import u8 card_read_block(u16 offset_hi, u16 offset_lo, u16 offset_in_block, u16 size, u8 *dst);
+import u8 card_read_block_u32(u8 *offset, u16 offset_in_block, u16 size, u8 *dst);
+
---
+++ app/lib/fat.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#define FAT_OK 0u8
+#define FAT_ERROR_CARD 1u8
+#define FAT_ERROR_UNSUPPORTED_TYPE 2u8
+#define FAT_ERROR_UNSUPPORTED_SECTOR_SIZE 3u8
+#define FAT_ERROR_TOO_MANY_FATS 4u8
+
+
+#define FAT_FILE_ATTR_READ_ONLY 1u8
+#define FAT_FILE_ATTR_HIDDEN 2u8
+#define FAT_FILE_ATTR_SYSTEM 4u8
+#define FAT_FILE_ATTR_VOLUME_ID 8u8
+#define FAT_FILE_ATTR_DIRECTORY 16u8
+#define FAT_FILE_ATTR_ARCHIVE 32u8
+#define FAT_FILE_ATTR_LONG_FILENAME 0x0Fu8
+struct FatDirEntry {
+    u8 filename[11];
+    u8 attrs;
+    u8 _reserved;
+    u8 create_dsec;
+    u16 create_time;
+    u16 create_date;
+    u16 access_date;
+    u16 cluster_hi; // 0x0000
+    u16 modify_time;
+    u16 modify_date;
+    u16 cluster_lo; // 0x0004
+    u8 size[4];
+};
+
+import u8 fat_init();
+
+#define FAT_BAD_DESC 0xFFu8
+/*
+ Returns dir descriptor or FAT_BAD_DESC on error.
+ */
+import u8 fat_open_dir();
+
+/*
+ Returns 1 on success, 0 on fail (or when no entries are left)
+ */
+import u8 fat_get_next_dir_entry(u8 dir_desc, struct FatDirEntry *dst);
+
+import u8 fat_close_dir(u8 dir_desc);
+
+import u8 fat_open_file(struct FatDirEntry *dsc);
+
+import u16 fat_read(u8 fd, u8 *dst, u16 len);
+
+import u8 fat_close_file(u8 fd);
