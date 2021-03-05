56960ef0634eac746022ebf1fa570f1269b518f3
Ivan Mikhailov <imihajlow@gmail.com>
2021-03-05 22:52:05 +0100

    Line edit

---
+++ app/lib/line_edit.h
@@ -0,0 +1,3 @@
+#pragma once
+
+import u8 line_edit(u8 *buf, u8 max_len, u8 min_screen_col, u8 screen_row, u8 color);
--- app/lib/string.h
+++ app/lib/string.h
@@ -3,3 +3,4 @@
 import u8 memset(u8 *b, u8 c, u16 len);
 import u8 memcpy(u8 *dst, u8 *src, u16 len);
 
+import u8 strlen_8(u8 *s);
