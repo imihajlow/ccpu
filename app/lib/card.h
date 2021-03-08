dda9891b61bd4b4057a34bd8cedb42536d42b397
Ivan Mikhailov <imihajlow@gmail.com>
2021-03-08 08:22:25 +0100

    Card: only read whole blocks

--- app/lib/card.h
+++ app/lib/card.h
@@ -14,6 +14,6 @@
 
 import u8 card_init();
 import u8 card_power_off();
-import u8 card_read_block(u32 offset, u16 offset_in_block, u16 size, u8 *dst);
+import u8 card_read_block(u32 offset, u8 *dst);
 import u8 card_write_block(u32 offset, u8 *src);
 
