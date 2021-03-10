c6a567c8e5bde13565831cd5adacbb556bb3fecd
Ivan Mikhailov <imihajlow@gmail.com>
2021-03-10 19:27:11 +0100

    FAT: faster file search, debug messages

---
+++ app/lib/log.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define DISABLE_LOG
+
+#ifndef DISABLE_LOG
+import u8 log_string(u8 *s);
+import u8 log_u8(u8 x);
+import u8 log_u16(u16 x);
+import u8 log_u32(u32 x);
+#else
+#define log_string(s)
+#define log_u8(s)
+#define log_u16(s)
+#define log_u32(s)
+#endif
