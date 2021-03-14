f70e907ae13dddd7383014670fe2f675ca357972
Ivan Mikhailov <imihajlow@gmail.com>
2021-03-14 10:09:43 +0100

    Loadable maze app, jump to any level

--- app/lib/entropy.h
+++ app/lib/entropy.h
@@ -7,3 +7,5 @@ import u16 entropy;
  * Same as keyboard_wait_key_released(), but user interaction is also used to get entropy.
  */
 import u8 keyboard_wait_key_released_entropy();
+
+import u8 ps2_wait_key_pressed_entropy();
