42e26c287b7b4a3ade51668720fad85eb0e2436b
Ivan Mikhailov <imihajlow@gmail.com>
2021-08-16 22:30:32 +0200

    Mandelbrot: fixed point implementation

--- app/lib/bcdf.h
+++ app/lib/bcdf.h
@@ -6,6 +6,11 @@ struct Bcdf {
     u8 man[14];
 };
 
+#define BCDF_SET_ZERO(x) memset((u8*)x, 0u8, sizeof(struct Bcdf))
+#define BCDF_SET_A(x) memcpy((u8*)&bcdf_op_a, (u8*)(x), sizeof(struct Bcdf))
+#define BCDF_SET_B(x) memcpy((u8*)&bcdf_op_b, (u8*)(x), sizeof(struct Bcdf))
+#define BCDF_GET_R(x) memcpy((u8*)(x), (u8*)&bcdf_op_r, sizeof(struct Bcdf))
+
 import struct Bcdf bcdf_op_a;
 import struct Bcdf bcdf_op_b;
 import struct Bcdf bcdf_op_r;
