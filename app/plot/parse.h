0be05347c5903ade96c427985cc067478449b3ae
Ivan Mikhailov <imihajlow@gmail.com>
2021-11-10 18:19:31 +0100

    Plot: actually plot

--- app/plot/parse.h
+++ app/plot/parse.h
@@ -2,7 +2,15 @@
 #include "tokenizer.h"
 
 
-// Returns bool
+#define PARSE_SUCCESS 0u8
+#define PARSE_ERR_OUT_OF_NODES 1u8
+#define PARSE_ERR_OUT_OF_OPERATORS 2u8
+#define PARSE_ERR_BRACKETS 3u8
+#define PARSE_ERR_TOO_FEW_OPERANDS 4u8
+#define PARSE_ERR_TOO_MANY_OPERANDS 5u8
+#define PARSE_ERR_UNKNOWN 255u8
+
+// Returns error code
 import u8 parse(u8 *str);
 import u8 print_tree();
 import struct Bcdf *evaluate(struct Bcdf *vars);
