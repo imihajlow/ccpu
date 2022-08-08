e933c3aaa1b684e6cfa831e3c04f1d2db2a47d5a
Ivan Mikhailov <imihajlow@gmail.com>
2022-08-08 20:22:18 +0200

    Z80: clean up hello world

--- app/z80/z80_apps/hello.c
+++ app/z80/z80_apps/hello.c
@@ -3,16 +3,11 @@
 
 void foo(char *dst, int x) {
     sprintf(dst, "Hello world! x = %d", x);
-    // for (int i = 0; i != x; ++i) {
-    //     strcpy(dst, "hello");
-    //     // *dst = 'H';
-    //     dst += 6;
-    // }
 }
 
 int main(void) {
     memset((char*)0xd000, 7, 128 * 30);
-    // strcpy((char*)0xe000, "Hello from Z80!");
+    memset((char*)0xe000, 0, 128 * 30);
     foo((char*)0xe000, 1234);
     foo((char*)0xe000 + 128, -500);
     return 0;
